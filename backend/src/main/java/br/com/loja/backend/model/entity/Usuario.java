e026f4ff5bee5fe467966871e716914daf23bbc8
isac.alves <isac.carmo@singletech.com.br>
2026-02-10 09:17:06 -0300

    feat: configura backend com postgres e docker

---
+++ backend/src/main/java/br/com/loja/backend/model/entity/Usuario.java
@@ -0,0 +1,24 @@
+package br.com.loja.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "usuarios")
+@Data
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private String role;
+}
+
